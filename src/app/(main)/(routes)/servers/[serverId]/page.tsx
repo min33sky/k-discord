0340d64ac8d862e23889462ba4e60a0f2b7b8daa
min33sky <min33sky@naver.com>
2023-09-11 15:22:13 +0900

    feat: add redirect general channel

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import { currentProfile } from '@/lib/current-profile';
+import prisma from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-export default function ServerIdPage() {
-  return <div>ServerIdPage</div>;
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+export default async function ServerIdPage({ params }: ChannelIdPageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await prisma.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'general',
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      },
+    },
+  });
+
+  //* 기본 채널 (general)이 있을 경우 리다이렉트 시키고 없으면 404
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== 'general') {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 }
